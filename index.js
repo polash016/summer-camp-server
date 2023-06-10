d4c42087f8e218d0f015d966cacfa4f244f64702
polash016 <robin.shrkr@gmail.com>
2023-06-10 21:38:17 +0600

    make role admin route created

--- index.js
+++ index.js
@@ -62,6 +62,19 @@ async function run() {
         res.send(result)
     })
 
+    app.patch('/users/admin/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const updatedDoc = {
+            $set: {
+                role: 'admin'
+            }
+        };
+        const result = await usersCollection.updateOne(query, updatedDoc);
+        res.send(result)
+    })
+    
+
 
     app.get('/classes', async(req, res) => {
         const result = await classCollection.find().sort({enrolled_students: -1}).toArray();

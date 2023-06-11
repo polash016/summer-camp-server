d77c1194ed4fa21c2115e8918da0185cdea9d6eb
polash016 <robin.shrkr@gmail.com>
2023-06-11 15:51:25 +0600

    verify admin done

--- index.js
+++ index.js
@@ -68,7 +68,15 @@ async function run() {
         res.send({token})
     })
 
-    
+    const verifyAdmin = async(req, res, next) => {
+        const email = req.decoded.email;
+        const query = {email: email};
+        const user = await usersCollection.findOne(query);
+        if(user.role !== 'admin'){
+            return res.status(403).send({error: true, message: 'Forbiddenn access'})
+        }
+        next()
+    }
     const verifyInstructor = async(req, res, next) => {
         const email = req.decoded.email;
         const query = {email: email};

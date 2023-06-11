2bd6eaaec0a960b93db27cf3a7051361c7de6888
polash016 <robin.shrkr@gmail.com>
2023-06-11 15:50:51 +0600

    jwt, admin route, instructor route added

--- index.js
+++ index.js
@@ -3,11 +3,28 @@ const app = express();
 const cors = require('cors');
 const { ObjectId } = require("mongodb");
 require('dotenv').config();
+var jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 
 app.use(cors());
 app.use(express.json())
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(!authorization){
+        return res.status(401).send({error: true, message: 'Unauthorized access'})
+    }
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+            return res.status(401).send({error: true, message: 'Unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 app.get('/', (req,res) => {
     res.send('Summer Camp Running')
@@ -45,7 +62,24 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
 
-    app.get('/users', async(req, res) => {
+    app.post('/jwt', (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+        res.send({token})
+    })
+
+    
+    const verifyInstructor = async(req, res, next) => {
+        const email = req.decoded.email;
+        const query = {email: email};
+        const user = await usersCollection.findOne(query);
+        if(user.role !== 'instructor'){
+            return res.status(403).send({error: true, message: 'Forbidden access'})
+        }
+        next()
+    }
+
+    app.get('/users',verifyJWT,verifyAdmin, async(req, res) => {
         const result = await usersCollection.find().toArray();
         res.send(result)
     })
@@ -54,11 +88,21 @@ async function run() {
         const user = req.body;
         const query = {email: user.email};
         const existingUser = await usersCollection.findOne(query);
-        console.log(user)
         if(existingUser){
             return res.send({message: 'User Already Exists'})
         }
         const result = await usersCollection.insertOne(user);
+        res.send(result)
+    })
+
+    app.get('/users/admin/:email', verifyJWT, async(req, res) => {
+        const email = req.params.email;
+        if(req.decoded.email !== email){
+            res.send({admin: false})
+        }
+        const query = {email: email};
+        const user = await usersCollection.findOne(query);
+        const result = {admin: user?.role === 'admin'};
         res.send(result)
     })
 
@@ -71,6 +115,19 @@ async function run() {
             }
         };
         const result = await usersCollection.updateOne(query, updatedDoc);
+        res.send(result)
+    })
+
+    //Instructor
+
+    app.get('/users/instructor/:email', verifyJWT, async(req, res) => {
+        const email = req.params.email;
+        if(req.decoded.email !== email){
+            res.send({instructor: false})
+        }
+        const query = {email: email};
+        const user = await usersCollection.findOne(query);
+        const result = {instructor: user?.role === 'instructor'};
         res.send(result)
     })
     
@@ -97,8 +154,17 @@ async function run() {
     })
 
     //selected class
-    app.get('/selectedClass', async(req, res) => {
-        const result = await selectedClassCollection.find().toArray()
+    app.get('/selectedClass', verifyJWT, async(req, res) => {
+        const email = req.query.email;
+        if(!email){
+            res.send([])
+        };
+        const decodedEmail = req.decoded.email;
+        if(email !== decodedEmail){
+            res.status(401).send({error: true, message: 'Forbidden access'})
+        };
+        const query = { email: email };
+        const result = await selectedClassCollection.find(query).toArray()
         res.send(result)
     })
     app.post('/selectedClass', async(req, res) => {
